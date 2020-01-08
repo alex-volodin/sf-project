76174e27ad24bc45b4c8e4cf825543d0fef3e861
Alexander Volodin <mr.stanlic@gmail.com>
2020-01-08 21:18:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+SF-Project
+
+This is my project for test Symfony's features. And although not, I'm just messing around)
---
+++ src/Controller/PageController.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @author    Alexander Volodin <mr-stanlik@yandex.ru>
+ */
+
+class PageController extends AbstractController
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request Request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function homepage(Request $request)
+    {
+        return $this->render('page:homepage.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request Request
+     * @param string                                    $slug    Slug
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function page(Request $request, string $slug)
+    {
+        return $this->render('page:page.html.twig', [
+
+        ]);
+    }
+}
